095e1a80841bab7a757153222fc71413ad9f189c
Joris Bontje <jorisbontje@gmail.com>
2011-06-17 14:19:41 +0200

    Extract KnmiLineParser

---
+++ hadoop-weather/src/main/java/com/xebia/summerclass/hadoop/weather/KnmiLineParser.java
@@ -0,0 +1,55 @@
+package com.xebia.summerclass.hadoop.weather;
+
+import org.apache.hadoop.io.Text;
+
+public class KnmiLineParser {
+
+
+    public static enum KnmiLineType {
+        COMMENT, EMPTY, DATA, UNPARSABLE;
+    }
+
+    public static enum KnmiLineField {
+        STN(0), YYYYMMDD(1), RH(22);
+
+        public final int position;
+
+        private KnmiLineField(int position) {
+            this.position = position;
+        }
+    }
+
+    private static int EXPECTED_FIELDS = 41;
+    private static String COMMENT_PREFIX = "#";
+
+    private String[] fields;
+
+    public KnmiLineType parse(Text line) {
+        return this.parse(line.toString());
+    }
+
+    public KnmiLineType parse(String line) {
+        if (line.startsWith(COMMENT_PREFIX)) {
+            return KnmiLineType.COMMENT;
+        }
+
+        this.fields = line.split(",\\s*");
+        if (this.fields.length != EXPECTED_FIELDS) {
+            return KnmiLineType.UNPARSABLE;
+        }
+
+        return KnmiLineType.DATA;
+    }
+
+    public String getStation() {
+        return this.fields[KnmiLineField.STN.position].trim();
+    }
+
+    public String getDate() {
+        return this.fields[KnmiLineField.YYYYMMDD.position].trim();
+    }
+
+    public long getPrecipitation() {
+        return Long.parseLong(this.fields[KnmiLineField.RH.position]);
+    }
+}
--- hadoop-weather/src/main/java/com/xebia/summerclass/hadoop/weather/mapreduce/rain/PrecipitationPerMonthMapper.java
+++ hadoop-weather/src/main/java/com/xebia/summerclass/hadoop/weather/mapreduce/rain/PrecipitationPerMonthMapper.java
@@ -7,38 +7,30 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.log4j.Logger;
 
+import com.xebia.summerclass.hadoop.weather.KnmiLineParser;
+import com.xebia.summerclass.hadoop.weather.KnmiLineParser.KnmiLineType;
+
 public class PrecipitationPerMonthMapper extends Mapper<LongWritable, Text, Text, LongWritable> {
 
     private static final Logger LOG = Logger.getLogger(PrecipitationPerMonthMapper.class);
 
-    private static String COMMENT_PREFIX = "#";
-    private static int STN = 0;
-    private static int YYYYMMDD = 1;
-    private static int RH = 22;
-    private static int EXPECTED_FIELDS = 41;
+    private KnmiLineParser parser = new KnmiLineParser();
 
     protected void map(LongWritable offset, Text line, Context context) throws IOException, InterruptedException {
         try {
-            String lineString = line.toString();
-            if (lineString.startsWith(COMMENT_PREFIX)) {
-                return;
+            if (parser.parse(line) == KnmiLineType.DATA) {
+                context.write(keyForStationAndDate(), valueForPrecipitation());
             }
-
-            String[] values = lineString.split(",\\s*");
-            if (values.length != EXPECTED_FIELDS) {
-                return;
-            }
-
-            long precipitation = Long.parseLong(values[RH]);
-            if (precipitation < 0) {
-                precipitation = 0;
-            }
-
-            Text key = new Text(values[STN].trim() + "," + values[YYYYMMDD].substring(0, 6));
-
-            context.write(key, new LongWritable(precipitation));
         } catch (Exception e) {
             LOG.warn("Exception caught during call to map(): ", e);
         }
+    }
+
+    private Text keyForStationAndDate() {
+        return new Text(parser.getStation() + "," + parser.getDate().substring(0, 6));
+    }
+
+    private LongWritable valueForPrecipitation() {
+        return new LongWritable(Math.max(parser.getPrecipitation(), 0));
     }
 }
